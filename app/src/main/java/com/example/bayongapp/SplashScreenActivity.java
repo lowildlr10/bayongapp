19775f50b323399341369f3c254564692541c317
Lowil Ray Delos Reyes <lowildlr10@gmail.com>
2022-01-28 02:46:59 +0800

    Developed the splash screen

--- app/src/main/java/com/example/bayongapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/bayongapp/SplashScreenActivity.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIMEOUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        //startActivity(new Intent(this, MainActivity.class));
 
-        startActivity(new Intent(this, MainActivity.class));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
 
     }
 }
