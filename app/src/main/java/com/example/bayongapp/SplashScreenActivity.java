bc2887458aaf8c52bc9b0d5b033c80a257fa2def
lowildlr10 <lowildlr10@gmail.com>
2022-01-27 17:38:14 +0800

    Developed initial splash screen.

--- app/src/main/java/com/example/bayongapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/bayongapp/SplashScreenActivity.java
@@ -2,14 +2,16 @@ package com.example.bayongapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+
+        startActivity(new Intent(this, MainActivity.class));
+
     }
 }
